477d69aa8afceea3d10db88209828abab36938f8
Sthuthi geetha Chilukuri <Sthuthigeetha1@gmail.com>
2024-07-29 12:03:30 +0000

    Print duplicate elements in an array.

---
+++ array.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int array[20], i, j, n;
+    printf("Enter array size");
+    scanf("%d",&n);
+     printf("Enter array elements\n");
+     
+     for(i=0;i<n;i++)
+     {
+        scanf("%d",&array[i]);
+     }
+     printf("The duplicate elements in an array are: \n");
+     
+        for(i=0;i<n;i++)
+        {
+            for(j=i+1;j<n;j++)
+            {
+                if(array[i]==array[j])
+                {
+                    printf("%d\t",array[j]);
+                }
+            }
+        }
+
+}
